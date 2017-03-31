c20243b54325541cb7ccd2325b27058523be0ac6
aymane <aymanbouziane@gmail.com>
2017-03-31 04:04:15 +0200

    first impl homeAcitivity and SearchActivity

---
+++ application/Flexin/app/src/main/java/com/crystal/flexin/impl/HomeActivity.java
@@ -0,0 +1,55 @@
+package com.crystal.flexin.impl;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+
+import com.crystal.flexin.TagViewer;
+
+import se.anyro.nfc_reader.R;
+
+public class HomeActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+        Button scanButton = (Button) findViewById(R.id.scanButton);
+        scanButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent intent = new Intent(HomeActivity.this, TagViewer.class);
+                startActivity(intent);
+            }
+        });
+
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        switch (item.getItemId()) {
+            case R.id.menu_search:
+                Intent intent = new Intent(HomeActivity.this, SearchActivity.class);
+                startActivity(intent);
+                return true;
+            case R.id.menu_home:
+                Intent intent2 = new Intent(HomeActivity.this, HomeActivity.class);
+                startActivity(intent2);
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+}
---
+++ application/Flexin/app/src/main/java/com/crystal/flexin/impl/SearchActivity.java
@@ -0,0 +1,14 @@
+package com.crystal.flexin.impl;
+
+import android.os.Bundle;
+
+import se.anyro.nfc_reader.R;
+
+public class SearchActivity extends HomeActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_search);
+    }
+}
